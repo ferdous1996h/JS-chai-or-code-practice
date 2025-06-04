2d68585562a26ed23fee8814a49aea732dfedd21
ferdous1996h <ferdous1996h@gmail.com>
2025-06-05 00:14:56 +0600

    check1

--- 01_Basics/Data type summary.js
+++ 01_Basics/Data type summary.js
@@ -1,18 +1,38 @@
-let first = Symbol('236');
-let ujiki = Symbol('236');
+// let first = Symbol('236');
+// let ujiki = Symbol('236');
 
-console.log(first === ujiki);
+// console.log(first === ujiki);
 
-const sup = ["spider", "homelander", "starlight"]
+// const sup = ["spider", "homelander", "starlight"]
 
-let myobj={
-  name : Ferdous ,
-  age : 25
-}
+// let myobj={
+//   name : Ferdous ,
+//   age : 25
+// }
 
-const myfuns = function () {
-  console.log("Hello World")
-}
+// const myfuns = function () {
+//   console.log("Hello World")
+// }
 
 //memory
 //Stack Memory(Primitive)////Heep Memory(Non Primitive)/////
+
+//stack memory
+let filemode = "ferdous456@gmail.com"
+let donkuholk = filemode
+donkuholk = "gonagirl"
+console.log(filemode);
+console.log(donkuholk);
+
+
+///heep memory
+
+let userone{
+  email: "ferdous89@gmail.com",
+  phone: 01478259656,
+}
+let usertwo = userone
+usertwo.email="ulala@gmail.com"
+
+console.log(userone)
+console.log(usertwo)
