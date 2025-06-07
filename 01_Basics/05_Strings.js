053b1d6191e2a29843255ba7d90d20161f2daa88
ferdous1996h <ferdous1996h@gmail.com>
2025-06-07 23:20:04 +0600

    math and n

--- 01_Basics/05_Strings.js
+++ 01_Basics/05_Strings.js
@@ -3,3 +3,31 @@ const age = 56
 // console.log(name + age)
 
 console.log(`My name is ${name} and my age is ${age}`)
+
+const mygame = new String("Ferdous-swcjkn")
+
+console.log(mygame[4]);
+console.log(mygame.length);
+console.log(mygame.toUpperCase())
+
+
+const girltake = new String("Robinhood-ulala0978")
+console.log(girltake.charAt(4));
+console.log(girltake.indexOf("-"));
+
+const Hrittik = new String("Rakib-foultillydfdbfefbesb")
+const Bilal = Hrittik.slice(-3, 2)
+console.log(Bilal);
+
+const Frstchk = "        How    "
+console.log(Frstchk);
+console.log(Frstchk.trim());
+
+
+const url = "https://ferdous.com/ferdous20%willdothat"
+console.log(url.replace("20%", "-"))
+console.log(url.includes("fer"))
+
+const rubbi = "Most-of-the-case"
+const phillow = rubbi
+console.log(phillow.split("-"));
---
+++ 01_Basics/06_Nums_and_Maths.js
@@ -0,0 +1,39 @@
+// let score = 100
+// console.log(score);
+
+// const balance = new Number(500)
+// console.log(balance);
+
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(3))
+
+
+// let othernumber = 169.253669
+// console.log(othernumber.toPrecision(2));
+
+
+// let wordsinnum = 10000000000000000
+// console.log(wordsinnum.toLocaleString());
+
+//+++++++++Maths+++++++++//
+
+// console.log(Math)
+// console.log(Math.abs(-86))
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.99));
+// console.log(Math.sqrt(36));
+// console.log(Math.max(22,55,33,17,18,57,13))
+// console.log(Math.min(22, 55, 33, 17, 18, 57, 13))
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random() * 10 + 1));
+
+const min = 25
+const max = 39
+
+console.log(Math.floor(Math.random() * (max - min) + 1)+min);
+
+
